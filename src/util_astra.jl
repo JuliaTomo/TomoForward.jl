bd385c2a919def33fa660f2273c0806457865af5
koo <jakeoung@users.noreply.github.com>
2020-02-05 23:49:52 +0100

    Update util_astra.jl

--- src/util_astra.jl
+++ src/util_astra.jl
@@ -1,3 +1,24 @@
+# This code is ported to Julia from ASTRA-toolbox
+# /*
+# -----------------------------------------------------------------------
+# Copyright: 2010-2018, imec Vision Lab, University of Antwerp
+#            2014-2018, CWI, Amsterdam
+# Contact: astra@astra-toolbox.com
+# Website: http://www.astra-toolbox.com/
+# This file is part of the ASTRA Toolbox.
+# The ASTRA Toolbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The ASTRA Toolbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with the ASTRA Toolbox. If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------
+# */
+
 function geom_2vec_parallel2d(DetectorWidth, ProjectionAngles)
     """ Ported from ASTRA toolbox
     https://github.com/astra-toolbox/astra-toolbox/blob/10d87f45bc9311c0408e4cacdec587eff8bc37f8/matlab/tools/astra_geom_2vec.m
