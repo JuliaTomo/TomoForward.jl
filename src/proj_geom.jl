9c01a49437f47c00e80d414599fe1077b2500c77
jakeoung <jakeoung@gmail.com>
2020-02-09 18:03:44 +0100

    fix the types in projgeom

--- src/proj_geom.jl
+++ src/proj_geom.jl
@@ -1,17 +1,15 @@
-# module proj_geom
-
 include("util_astra.jl")
-export ProjGeom
 
 """Projection geometry structure
 
+    struct ProjGeom {T<:AbstractFloat}
     Type::String
-    DetectorSpacingX::AbstractFloat
-    DetectorSpacingY::AbstractFloat
+    DetectorSpacingX::T 
+    DetectorSpacingY::T
     DetectorColCount::Int
     DetectorRowCount::Int
-    ProjectionAngles::Array{AbstractFloat,1}
-    Vectors::Array{AbstractFloat,2}
+    ProjectionAngles::Array{T,1}
+    Vectors::Array{T,2}
 
     # parallel 2d
     ProjGeom(spacing, detcount, angles) = new("parallel2d", spacing, 0.0, detcount, 0, angles, geom_2vec_parallel2d(b, c))
@@ -22,24 +20,21 @@ export ProjGeom
     # cone
     ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles, source_origin, origin_det) = new("parallel3d", detspacingx, detspacingy, detcolcount, detrowcount, geom_2vec_cone(detspacingx, detspacingy, angles, source_origin, origin_det))
 """
-struct ProjGeom
+struct ProjGeom {T<:AbstractFloat}
     Type::String
-    DetectorSpacingX::AbstractFloat # (@don't need to save) already inherited in Vectors
-    DetectorSpacingY::AbstractFloat
+    DetectorSpacingX::T 
+    DetectorSpacingY::T
     DetectorColCount::Int
     DetectorRowCount::Int
-    ProjectionAngles::Array{AbstractFloat,1}
-    Vectors::Array{AbstractFloat,2}
-        
-    # parallel 2d
-    ProjGeom(spacing, detcount, angles) = new("parallel2d", spacing, 0.0, detcount, 0, angles, geom_2vec_parallel2d(spacing, angles))
-    
-    # parallel 3d
-    ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles) = new("parallel3d", detspacingx, detspacingy, detcolcount, detrowcount, angles, geom_2vec_parallel3d(detspacingx, detspacingy, angles))
-    
-    # cone
-    ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles, source_origin, origin_det) = new("parallel3d", detspacingx, detspacingy, detcolcount, detrowcount, angles, geom_2vec_cone(detspacingx, detspacingy, angles, source_origin, origin_det))
-    
+    ProjectionAngles::Array{T,1}
+    Vectors::Array{T,2}
 end
 
-# end
+# parallel 2d
+ProjGeom(spacing, detcount, angles) = new("parallel2d", spacing, 0.0, detcount, 0, angles, geom_2vec_parallel2d(spacing, angles))
+    
+# parallel 3d
+ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles) = new("parallel3d", detspacingx, detspacingy, detcolcount, detrowcount, angles, geom_2vec_parallel3d(detspacingx, detspacingy, angles))
+
+# cone
+ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles, source_origin, origin_det) = new("cone", detspacingx, detspacingy, detcolcount, detrowcount, angles, geom_2vec_cone(detspacingx, detspacingy, angles, source_origin, origin_det))
