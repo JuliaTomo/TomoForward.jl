726e492eba1b653da5d9859568556b555345ef27
jakeoung <jakeoung@gmail.com>
2020-02-09 18:38:14 +0100

    fix ProjGeom

--- src/proj_geom.jl
+++ src/proj_geom.jl
@@ -3,13 +3,15 @@ include("util_astra.jl")
 """Projection geometry structure
 
     struct ProjGeom {T<:AbstractFloat}
-    Type::String
-    DetectorSpacingX::T 
-    DetectorSpacingY::T
-    DetectorColCount::Int
-    DetectorRowCount::Int
-    ProjectionAngles::Array{T,1}
-    Vectors::Array{T,2}
+        Type::String
+        DetectorSpacingX::T 
+        DetectorSpacingY::T
+        DetectorColCount::Int
+        DetectorRowCount::Int
+        ProjectionAngles::Array{T,1}
+        Vectors::Array{T,2}
+        DistanceOriginSource
+        DistanceOriginDetector
 
     # parallel 2d
     ProjGeom(spacing, detcount, angles) = new("parallel2d", spacing, 0.0, detcount, 0, angles, geom_2vec_parallel2d(b, c))
@@ -32,13 +34,11 @@ struct ProjGeom{T<:AbstractFloat}
     DistanceOriginDetector
     
     # parallel 2d
-    ProjGeom(spacing, detcount, angles) = ProjGeom("parallel2d", spacing, 0.0, detcount, 0, Array{T}(angles), geom_2vec_parallel2d(spacing, angles), nothing, nothing)
+    ProjGeom(spacing, detcount, angles) = ProjGeom("parallel2d", spacing, 0.0, detcount, 0, Array{Float64}(angles), geom_2vec_parallel2d(spacing, angles), nothing, nothing)
         
     # parallel 3d
-    ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles) = ProjGeom("parallel3d", detspacingx, detspacingy, detcolcount, detrowcount, Array{T}(angles), geom_2vec_parallel3d(detspacingx, detspacingy, angles), nothing, nothing)
+    ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles) = ProjGeom("parallel3d", detspacingx, detspacingy, detcolcount, detrowcount, Array{Float64}(angles), geom_2vec_parallel3d(detspacingx, detspacingy, angles), nothing, nothing)
 
     # cone
-    ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles, source_origin, origin_det) = ProjGeom("cone", detspacingx, detspacingy, detcolcount, detrowcount, Array{T}(angles), geom_2vec_cone(detspacingx, detspacingy, angles, source_origin, origin_det), nothing, nothing)
+    ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles, source_origin, origin_det) = ProjGeom("cone", detspacingx, detspacingy, detcolcount, detrowcount, Array{Float64}(angles), geom_2vec_cone(detspacingx, detspacingy, angles, source_origin, origin_det), source_origin, origin_det)
 end
-
-ProjGeom(spacing, detcount, angles::StepRangeLen) = ProjGeom(spacing, detcount, Array(angles))
