6c737869d82f44c524b3f70a857547dcbcee3e56
jakeoung <jakeoung@gmail.com>
2020-02-09 20:25:45 +0100

    fix ProjGeom

--- src/proj_geom.jl
+++ src/proj_geom.jl
@@ -22,7 +22,7 @@ include("util_astra.jl")
     "cone"
     ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles, source_origin, origin_det) = ProjGeom("cone", detspacingx, detspacingy, detcolcount, detrowcount, Array{Float64}(angles), geom_2vec_cone(detspacingx, detspacingy, angles, source_origin, origin_det), source_origin, origin_det)
 """
-struct ProjGeom{T<:AbstractFloat}
+mutable struct ProjGeom{T<:AbstractFloat}
     Type::String
     DetectorSpacingX::T 
     DetectorSpacingY::T
