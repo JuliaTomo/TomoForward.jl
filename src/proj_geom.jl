0eb33b0ddc61656283e21175bb04322462520e1e
jakeoung <jakeoung@gmail.com>
2020-02-09 18:09:10 +0100

    fix ProjGeom

--- src/proj_geom.jl
+++ src/proj_geom.jl
@@ -31,10 +31,10 @@ struct ProjGeom{T<:AbstractFloat}
 end
 
 # parallel 2d
-ProjGeom(spacing, detcount, angles) = new("parallel2d", spacing, 0.0, detcount, 0, angles, geom_2vec_parallel2d(spacing, angles))
+ProjGeom(spacing, detcount, angles) = ProjGeom("parallel2d", spacing, 0.0, detcount, 0, angles, geom_2vec_parallel2d(spacing, angles))
     
 # parallel 3d
-ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles) = new("parallel3d", detspacingx, detspacingy, detcolcount, detrowcount, angles, geom_2vec_parallel3d(detspacingx, detspacingy, angles))
+ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles) = ProjGeom("parallel3d", detspacingx, detspacingy, detcolcount, detrowcount, angles, geom_2vec_parallel3d(detspacingx, detspacingy, angles))
 
 # cone
-ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles, source_origin, origin_det) = new("cone", detspacingx, detspacingy, detcolcount, detrowcount, angles, geom_2vec_cone(detspacingx, detspacingy, angles, source_origin, origin_det))
+ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles, source_origin, origin_det) = ProjGeom("cone", detspacingx, detspacingy, detcolcount, detrowcount, angles, geom_2vec_cone(detspacingx, detspacingy, angles, source_origin, origin_det))
