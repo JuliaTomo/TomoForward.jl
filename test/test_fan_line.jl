9b3621aa40a815b131aae88498a7ede6808e806a
jakeoung <jakeoung@gmail.com>
2020-02-18 00:23:57 +0200

    add fan

--- test/test_fan_line.jl
+++ test/test_fan_line.jl
@@ -8,7 +8,7 @@ detcount = Int(floor(size(img,1)*1.4))
 src_origin = 800.0
 det_origin = 500.0
 angles = LinRange(0,pi,nangles+1)[1:nangles]
-proj_geom = ProjGeom(1.0, detcount, Array(angles), src_origin, det_origin)
+proj_geom = ProjGeomFan(1.0, detcount, angles, src_origin, det_origin)
 
 # test line projection model
 A = fp_op_fan_line(proj_geom, size(img, 1), size(img, 2))
