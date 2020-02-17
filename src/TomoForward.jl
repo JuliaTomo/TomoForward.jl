8e0696836e358bf64f73e8d831b646fa37f0b5b0
jakeoung <jakeoung@gmail.com>
2020-02-17 23:45:47 +0200

    add fan beam

--- src/TomoForward.jl
+++ src/TomoForward.jl
@@ -8,7 +8,8 @@ export ProjGeom
 include("fp_op_common.jl")
 include("fp_op_parallel2d_line.jl")
 include("fp_op_parallel2d_strip.jl")
+include("fp_op_fan_line.jl")
 
-export fp_op_parallel2d_line, fp_op_parallel2d_strip
+export fp_op_parallel2d_line, fp_op_parallel2d_strip, fp_op_fan_line
 
 end
