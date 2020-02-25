6a970e0190b7bda3ceb3407fd04b1343f4626deb
jakeoung <jakeoung@gmail.com>
2020-02-25 20:52:04 +0100

    support vectors

--- src/proj_geom.jl
+++ src/proj_geom.jl
@@ -36,7 +36,7 @@ end
 "parallel 2d"
 ProjGeom(spacing, detcount, angles) = ProjGeom("parallel2d", spacing, 0.0, detcount, 0, Array{Float64}(angles), geom_2vec_parallel2d(spacing, angles))
 
-ProjGeom(spacing, detcount, vectors) = ProjGeom("parallel2d", spacing, 0.0, detcount, 0, Float64[], vectors)
+ProjGeom(detcount, vectors) = ProjGeom("parallel2d", -1.0, 0.0, detcount, 0, Float64[], vectors)
 
 # "parallel 3d"
 # ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles) = ProjGeom("parallel3d", detspacingx, detspacingy, detcolcount, detrowcount, Array(angles), geom_2vec_parallel3d(detspacingx, detspacingy, angles))
@@ -44,7 +44,7 @@ ProjGeom(spacing, detcount, vectors) = ProjGeom("parallel2d", spacing, 0.0, detc
 "fan"
 ProjGeomFan(spacing, detcount, angles, src_origin, det_origin) = ProjGeom("fan", spacing, 0.0, detcount, 0, Array{Float64}(angles), geom_2vec_fan(spacing, angles, src_origin, det_origin))
 
-ProjGeomFan(spacing, detcount, vectors) = ProjGeom("fan", spacing, 0.0, detcount, 0, Float64[], vectors)
+ProjGeomFan(detcount, vectors) = ProjGeom("fan", -1.0, 0.0, detcount, 0, Float64[], vectors)
 
 # "cone"
 # ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles, source_origin, origin_det) = ProjGeom("cone", detspacingx, detspacingy, detcolcount, detrowcount, Array(angles), geom_2vec_cone(detspacingx, detspacingy, angles, source_origin, origin_det))
