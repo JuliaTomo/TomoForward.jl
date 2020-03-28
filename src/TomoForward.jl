11381ba831981751442aa9a2bb8988dea30b4aed
jakeoung <jakeoung@gmail.com>
2020-03-28 16:09:28 +0100

    add fp 2d slices

--- src/TomoForward.jl
+++ src/TomoForward.jl
@@ -9,7 +9,10 @@ include("fp_op_common.jl")
 include("fp_op_parallel2d_line.jl")
 include("fp_op_parallel2d_strip.jl")
 include("fp_op_fan_line.jl")
-
 export fp_op_parallel2d_line, fp_op_parallel2d_strip, fp_op_fan_line
+
+
+include("fp_op_utils.jl")
+export fp_2d_slices!
 
 end
