97f94624330bfc66e8843f9b88f094198a1039a3
jakeoung <jakeoung@gmail.com>
2020-05-20 21:50:20 +0200

    minor

--- src/TomoForward.jl
+++ src/TomoForward.jl
@@ -15,4 +15,7 @@ export fp_op_parallel2d_line, fp_op_parallel2d_strip, fp_op_fan_line
 include("fp_op_utils.jl")
 export fp_2d_slices!
 
+# util_astra
+export geom_2vec_parallel2d, geom_2vec_parallel3d, geom_2vec_fan, geom_2vec_cone
+
 end
