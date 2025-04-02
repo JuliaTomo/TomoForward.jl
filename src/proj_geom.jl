9c34c77fa3b765949928dca5f05b5df1a24aea50
JaKeoung Koo <jakeoung@users.noreply.github.com>
2025-04-03 01:03:09 +0900

    Update proj_geom.jl

--- src/proj_geom.jl
+++ src/proj_geom.jl
@@ -47,7 +47,7 @@ ProjGeomFan(spacing, detcount, angles, src_origin, det_origin) = ProjGeom("fan",
 ProjGeomFan(detcount, vectors) = ProjGeom("fan", -1.0, 0.0, detcount, 0, Float64[], vectors)
 
 # "cone"
-ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles, source_origin, origin_det) = ProjGeom("cone", detspacingx, detspacingy, detcolcount, detrowcount, Array(angles), geom_2vec_cone(detspacingx, detspacingy, angles, source_origin, origin_det))
+# ProjGeom(detspacingx, detspacingy, detrowcount, detcolcount, angles, source_origin, origin_det) = ProjGeom("cone", detspacingx, detspacingy, detcolcount, detrowcount, Array(angles), geom_2vec_cone(detspacingx, detspacingy, angles, source_origin, origin_det))
 
 # for initialization with vectors
 ProjGeom(type::String) = ProjGeom(type, nothing, nothing, 0, 0, nothing, nothing)
